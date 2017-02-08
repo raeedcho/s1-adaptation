06b823c0d7c5048a496ea4b030c5e764d6a8cef1
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-02-08 14:40:08 -0600

    offload some stuff into scripts for bumpcurl analysis

--- lib/extract_bumpcurl_data.m
+++ lib/extract_bumpcurl_data.m
@@ -1,12 +1,12 @@
-function [data,trial_table] = extract_bumpcurl_data(input_struct)
+function [binned_data,emg,trial_table] = extract_bumpcurl_data(cds)
 
-% Make CDS file
-cds = commonDataStructure();
-cds.file2cds([input_struct.folder filesep input_struct.fname],input_struct.ranBy,input_struct.array,input_struct.monkey,input_struct.lab,'ignoreJumps',input_struct.task,input_struct.mapfile);
-
-if(isfield(input_struct,'save_folder'))
-    save(input_struct.save_folder,'cds');
-end
+% % Make CDS file
+% cds = commonDataStructure();
+% cds.file2cds([input_struct.folder filesep input_struct.fname],input_struct.ranBy,input_struct.array,input_struct.monkey,input_struct.lab,'ignoreJumps',input_struct.task,input_struct.mapfile);
+% 
+% if(isfield(input_struct,'save_folder'))
+%     save(input_struct.save_folder,'cds','-v7.3');
+% end
 
 
 %% Extract trial table
@@ -22,20 +22,30 @@ ex.meta.hasForce=true;
 ex.meta.hasUnits=true;
 ex.meta.hasTrials=true;
 ex.meta.hasAnalog=true;
+ex.meta.hasEmg=true;
 % add session to experiment
 ex.addSession(cds);
-% Bin experiment data
+
+ex.emg.processDefault();
+
+%% extract emg
+emg = ex.emg.data;
+
+%% Bin experiment data
 ex.binConfig.include(1).field='units';
 ex.binConfig.include(1).which=find([ex.units.data.ID]==0);
 ex.binConfig.include(2).field='kin';
 ex.binConfig.include(2).which={};
 ex.binConfig.include(3).field='force';
 ex.binConfig.include(3).which={};
-% base_ex.binConfig.include(3).field='analog';
-% base_ex.binConfig.include(3).which=base_ex.analog(3).data.Properties.VariableNames(2:end);%kinect data
+ex.binConfig.include(4).field='emg';
+ex.binConfig.include(4).which={};
+ex.binConfig.include(5).field='analog';
+ex.binConfig.include(5).which={'KinectSyncPulse'};
+% base_ex.binConfig.include(3).which=base_ex.analog(2).data.Properties.VariableNames(2:end);%kinect data
 ex.firingRateConfig.cropType='tightCrop';
 ex.firingRateConfig.offset=-0.015;
 
 ex.binData()
 
-data = ex.bin.data;
+binned_data = ex.bin.data;
---
+++ lib/extract_extra_bumpcurl_data.m
@@ -0,0 +1,39 @@
+function [data,emg] = extract_extra_bumpcurl_data(cds)
+
+% Make CDS file
+% cds = commonDataStructure();
+% cds.file2cds([input_struct.folder filesep input_struct.fname],input_struct.ranBy,input_struct.array,input_struct.monkey,input_struct.lab,'ignoreJumps',input_struct.task,input_struct.mapfile);
+% 
+% if(isfield(input_struct,'save_folder'))
+%     save(input_struct.save_folder,'cds','-v7.3');
+% end
+
+%% Move CDS into Experiment class
+ex = experiment();
+% set variables to load from cds
+ex.meta.hasLfp=false;
+ex.meta.hasKinematics=false;
+ex.meta.hasForce=false;
+ex.meta.hasUnits=false;
+ex.meta.hasTrials=false;
+ex.meta.hasAnalog=true;
+ex.meta.hasEmg=true;
+% add session to experiment
+ex.addSession(cds);
+
+ex.emg.processDefault();
+
+%% extract emg
+emg = ex.emg.data;
+
+%% Bin experiment data
+ex.binConfig.include(1).field='emg';
+ex.binConfig.include(1).which={};
+ex.binConfig.include(2).field='analog';
+ex.binConfig.include(2).which={'Sync'};
+ex.firingRateConfig.cropType='tightCrop';
+ex.firingRateConfig.offset=-0.015;
+
+ex.binData()
+
+data = ex.bin.data;
