e449795b0663c0a78fa316f7662b6c104cbe63f5
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-02-20 16:41:23 -0600

    Delete some unneeded files

--- lib/save_bumpcurl_cds.m
+++
@@ -1,9 +0,0 @@
-function [cds] = save_bumpcurl_cds(input_struct)
-
-% Make CDS file
-cds = commonDataStructure();
-cds.file2cds([input_struct.folder filesep input_struct.fname],input_struct.ranBy,input_struct.array,input_struct.monkey,input_struct.lab,'ignoreJumps',input_struct.task,input_struct.mapfile);
-
-if(isfield(input_struct,'save_folder'))
-    save(input_struct.save_folder,'cds','-v7.3');
-end
