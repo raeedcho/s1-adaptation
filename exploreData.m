dd88041c359cb3e104d81069ddba9a4278f17c94
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2017-02-21 18:54:20 -0600

    add S1 analysis to bumpcurl stuff

--- exploreData.m
+++ exploreData.m
@@ -62,7 +62,7 @@ td = trialAverage(td,{'bumpDir','epoch'});
 epoch_names = {'BL','AD','WO'};
 figure;
 
-for epochCtr = 1:length(epoch_names);
+for epochCtr = 1:length(epoch_names)
     subplot(1,3,epochCtr)
     hold all;
     for i = getTDidx(td,'epoch',epoch_names(epochCtr))
@@ -76,12 +76,10 @@ end
 %% Take a look at kinematics
 
 td = truncateAndBin(trial_data_R,{'idx_bumpTime',-10},{'idx_bumpTime',50});
-% td = trialAverage(td,'bumpDir');
-
-animateBumpcurlTrials(td,'pos',bump_colors,0.01,5);
-
-% trial average
 % td = trialAverage(td,{'bumpDir','epoch'});
+
+% animateBumpcurlTrials(td,'pos',bump_colors,0.01,5);
+
 
 % set up plot
 % epoch_names = {'BL','AD','WO'};
@@ -97,131 +95,34 @@ animateBumpcurlTrials(td,'pos',bump_colors,0.01,5);
 %     end
 %     axis equal
 % end
-% neurIDX = contains(base_data.Properties.VariableNames,'LeftS1Area2CH');
-% base_bumps = [];
-% for trialCtr=1:height(base_table)
-%     base_bumps = [base_bumps; base_data{base_data.t>=base_table.bumpTime(trialCtr) & base_data.t<=base_table.bumpTime(trialCtr)+0.3,neurIDX}];
-% end
-% 
-% neurIDX = contains(adapt_data.Properties.VariableNames,'LeftS1Area2CH');
-% adapt_bumps = [];
-% for trialCtr=1:height(adapt_table)
-%     adapt_bumps = [adapt_bumps; adapt_data{adapt_data.t>=adapt_table.bumpTime(trialCtr) & adapt_data.t<=adapt_table.bumpTime(trialCtr)+0.3,neurIDX}];
-% end
-% 
-% neurIDX = contains(wash_data.Properties.VariableNames,'LeftS1Area2CH');
-% wash_bumps = [];
-% for trialCtr=1:height(wash_table)
-%     wash_bumps = [wash_bumps; wash_data{wash_data.t>=wash_table.bumpTime(trialCtr) & wash_data.t<=wash_table.bumpTime(trialCtr)+0.3,neurIDX}];
-% end
-% 
-% [coeff,scores,latent_eigs] = pca(sqrt([base_bumps;adapt_bumps;wash_bumps]));
-% % [base_coeff,base_scores,base_scree] = pca([base_bumps]);
-% 
-% % project adapt and wash bumps into pc space
-% % center
-% % adapt_bumps_ctr = adapt_bumps - mean(adapt_bumps);
-% % wash_bumps_ctr = wash_bumps - mean(wash_bumps);
-% % 
-% % % project
-% % adapt_scores = adapt_bumps_ctr*base_coeff;
-% % wash_scores = wash_bumps_ctr*base_coeff;
-% % 
-% 
-% 
-% base_idx = (1:size(base_bumps,1))';
-% adapt_idx = (base_idx(end)+1:base_idx(end)+size(adapt_bumps,1))';
-% wash_idx = (adapt_idx(end)+1:adapt_idx(end)+size(wash_bumps,1))';
-% 
-% base_scores = scores(base_idx,:);
-% adapt_scores = scores(adapt_idx,:);
-% wash_scores = scores(wash_idx,:);
-% 
-% base_scree = var(base_scores);
-% adapt_scree = var(adapt_scores);
-% wash_scree = var(wash_scores);
-% 
-% figure
-% subplot(311)
-% bar(base_scree(1:20))
-% subplot(312)
-% bar(adapt_scree(1:20))
-% subplot(313)
-% bar(wash_scree(1:20))
-% 
-% 
-% %% Look at PCs time traces
-% 
-% 
-% %% Plot color bumps
-% num_bumps = 4;
-% 
-% bump_sep = 360/num_bumps;
-% 
-% colors = linspecer(num_bumps);
-% base_colors = colors(floor(base_table.bumpDir/bump_sep+1),:);
-% adapt_colors = colors(floor(adapt_table.bumpDir/bump_sep+1),:);
-% wash_colors = colors(floor(wash_table.bumpDir/bump_sep+1),:);
-% 
-% figure
-% S = linspace(1,200,size(base_scores,1));
-% for j = 1:6
-%     subplot(2,3,j)
-%     for i = 1:size(base_colors,1)
-% %         idx = (i-1)*6+1:i*6;
-% %         idx = i*6-3;
-%         idx = (i-1)*6+j;
-% %         plot3(base_scores(idx,1),base_scores(idx,2),base_scores(idx,3),'Color',base_colors(i,:))
-% %         plot(base_scores(idx,1),base_scores(idx,2),'o','Color',base_colors(i,:))
-%         hold on
-%         scatter3(base_scores(idx,1),base_scores(idx,2),base_scores(idx,3),S(i),base_colors(i,:))
-%     end
-%     grid off
-%     axis square
-% end
-% 
-% 
-% figure
-% S = linspace(1,200,size(adapt_scores,1));
-% for j = 1:6
-%     subplot(2,3,j)
-%     for i = 1:size(adapt_colors,1)
-%         
-% %         idx = (i-1)*6+1:i*6;
-% %         idx = i*6-3;
-%         idx = (i-1)*6+j;
-% %         plot3(adapt_scores(idx,1),adapt_scores(idx,2),adapt_scores(idx,3),'Color',adapt_colors(i,:))
-% %         plot(adapt_scores(idx,1),adapt_scores(idx,2),'o','Color',adapt_colors(i,:))
-%         hold on
-% %         scatter(adapt_scores(idx,1),adapt_scores(idx,2),S(i),adapt_colors(i,:))
-%         scatter3(adapt_scores(idx,1),adapt_scores(idx,2),adapt_scores(idx,3),S(i),adapt_colors(i,:))
-%         
-%     end
-%     grid off
-%     axis square
-% end
-% 
-% figure
-% S = linspace(1,200,size(wash_scores,1));
-% for j = 1:6
-%     subplot(2,3,j)
-%     for i = 1:size(wash_colors,1)
-% %         idx = (i-1)*6+1:i*6;
-% %         idx = i*6-3;
-%         idx = (i-1)*6+j;
-% %         plot3(wash_scores(idx,1),wash_scores(idx,2),wash_scores(idx,3),'Color',wash_colors(i,:))
-% %         plot(wash_scores(idx,1),wash_scores(idx,2),'o','Color',wash_colors(i,:))
-%         hold on
-% %         scatter(wash_scores(idx,1),wash_scores(idx,2),S(i),wash_colors(i,:))
-%         scatter3(wash_scores(idx,1),wash_scores(idx,2),wash_scores(idx,3),S(i),wash_colors(i,:))
-%     end
-%     grid off
-%     axis square
-% end
-% 
-% 
-% 
-% %% Try mahalanobis distance on time-spread PCA
+
+%% S1 pca
+td = truncateAndBin(trial_data_R,{'idx_bumpTime',-10},{'idx_bumpTime',50});
+% td = trialAverage(td,{'bumpDir','epoch'});
+
+[td,pca_info] = getPCA(td,struct('signals','S1_spikes','sqrt_transform',true,'do_smoothing',true));
+
+% trial average
+td = trialAverage(td,{'bumpDir','epoch'});
+
+% set up plot
+epoch_names = {'BL','AD','WO'};
+figure;
+
+for epochCtr = 1:length(epoch_names)
+    subplot(1,3,epochCtr)
+    hold all;
+    for i = getTDidx(td,'epoch',epoch_names(epochCtr))
+        bumpDir_idx = td(i).bumpDir/90+1; % only works for 4 bump files
+        plot3(td(i).S1_spikes_pca(:,1),td(i).S1_spikes_pca(:,2),td(i).S1_spikes_pca(:,3),'k-','linewidth',2,'Color',bump_colors(bumpDir_idx,:))
+%         plot(td(i).S1_spikes_pca(:,1),td(i).S1_spikes_pca(:,2),'k-','linewidth',2,'Color',bump_colors(bumpDir_idx,:))
+    end
+    axis equal
+end
+
+animateBumpcurlTrials(td,'S1_spikes_pca',bump_colors,0.1,60);
+
+%% Try mahalanobis distance on time-spread PCA
 % base_clusters = cell(8,1);
 % for i = 1:8
 %     base_clusters{i} = base_scores(base_table.bumpDir/45+1==i,1:12);
@@ -312,274 +213,4 @@ animateBumpcurlTrials(td,'pos',bump_colors,0.01,5);
 % 
 % figure
 % plot(adapt_base_angles*180/pi)
-% 
-% 
-% %% Look at behavior
-% % kinIDX = [4 5];
-% kinIDX = [10 11];
-% base_bumps = [];
-% for trialCtr=1:height(base_table)
-%     base_bumps = [base_bumps; base_data{base_data.t>=base_table.bumpTime(trialCtr) & base_data.t<=base_table.bumpTime(trialCtr)+0.3,kinIDX}];
-% end
-% 
-% adapt_bumps = [];
-% for trialCtr=1:height(adapt_table)
-%     adapt_bumps = [adapt_bumps; adapt_data{adapt_data.t>=adapt_table.bumpTime(trialCtr) & adapt_data.t<=adapt_table.bumpTime(trialCtr)+0.3,kinIDX}];
-% end
-% 
-% wash_bumps = [];
-% for trialCtr=1:height(wash_table)
-%     wash_bumps = [wash_bumps; wash_data{wash_data.t>=wash_table.bumpTime(trialCtr) & wash_data.t<=wash_table.bumpTime(trialCtr)+0.3,kinIDX}];
-% end
-% 
-% num_bumps = 4;
-% 
-% bump_sep = 360/num_bumps;
-% 
-% colors = linspecer(num_bumps);
-% base_colors = colors(floor(base_table.bumpDir/bump_sep+1),:);
-% adapt_colors = colors(floor(adapt_table.bumpDir/bump_sep+1),:);
-% wash_colors = colors(floor(wash_table.bumpDir/bump_sep+1),:);
-% 
-% figure
-% S = linspace(1,40,size(base_bumps,1));
-% for j = 1:6
-%     subplot(2,3,j)
-%     for i = 1:size(base_colors,1)
-% %         idx = (i-1)*6+1:i*6;
-% %         idx = i*6-3;
-%         idx = (i-1)*6+j;
-% %         plot3(base_scores(idx,1),base_scores(idx,2),base_scores(idx,3),'Color',base_colors(i,:))
-% %         plot(base_scores(idx,1),base_scores(idx,2),'o','Color',base_colors(i,:))
-%         hold on
-%         scatter(base_bumps(idx,1),base_bumps(idx,2),S(i),base_colors(i,:))
-%     end
-%     grid off
-% %     axis([-30 30 -30 30])
-% %     axis([-5 5 -40 -30])
-% end
-% 
-% 
-% figure
-% S = linspace(1,40,size(adapt_bumps,1));
-% for j = 1:6
-%     subplot(2,3,j)
-%     for i = size(adapt_colors,1)-150:size(adapt_colors,1)
-%         
-% %         idx = (i-1)*6+1:i*6;
-% %         idx = i*6-3;
-%         idx = (i-1)*6+j;
-% %         plot3(adapt_scores(idx,1),adapt_scores(idx,2),adapt_scores(idx,3),'Color',adapt_colors(i,:))
-% %         plot(adapt_scores(idx,1),adapt_scores(idx,2),'o','Color',adapt_colors(i,:))
-%         hold on
-%         scatter(adapt_bumps(idx,1),adapt_bumps(idx,2),S(i),adapt_colors(i,:))
-% %         scatter3(adapt_scores(idx,1),adapt_scores(idx,2),adapt_scores(idx,3),S(i),adapt_colors(i,:))
-%         
-%     end
-%     grid off
-% %     axis([-30 30 -30 30])
-% %     axis([-5 5 -40 -30])
-% end
-% 
-% figure
-% S = linspace(1,40,size(wash_bumps,1));
-% for j = 1:6
-%     subplot(2,3,j)
-%     for i = 1:size(wash_colors,1)
-% %         idx = (i-1)*6+1:i*6;
-% %         idx = i*6-3;
-%         idx = (i-1)*6+j;
-% %         plot3(wash_scores(idx,1),wash_scores(idx,2),wash_scores(idx,3),'Color',wash_colors(i,:))
-% %         plot(wash_scores(idx,1),wash_scores(idx,2),'o','Color',wash_colors(i,:))
-%         hold on
-%         scatter(wash_bumps(idx,1),wash_bumps(idx,2),S(i),wash_colors(i,:))
-%         
-%     end
-%     grid off
-% %     axis([-30 30 -30 30])
-% %     axis([-5 5 -40 -30])
-% end
-% 
-% 
-% %% Look at EMG PEH
-% base_times = cell(1,4);
-% adapt_times = cell(1,4);
-% wash_times = cell(1,4);
-% for bumpCtr = 1:4
-%     base_times{bumpCtr} = base_table.bumpTime(base_table.bumpDir==(bumpCtr-1)*90);
-%     adapt_times{bumpCtr} = adapt_table.bumpTime(adapt_table.bumpDir==(bumpCtr-1)*90);
-%     wash_times{bumpCtr} = wash_table.bumpTime(wash_table.bumpDir==(bumpCtr-1)*90);
-% end
-% 
-% 
-% num_bumps = 4;
-% 
-% bump_sep = 360/num_bumps;
-% 
-% colors = linspecer(num_bumps);
-% 
-% for emgCtr = 2:width(base_emg)
-%     figure
-%     title(base_emg.Properties.VariableNames{emgCtr});
-%     xwindow = [-0.1 0.3];
-%     for i = 1:4
-%         [base_av,baseCIlow,baseCIhigh] = getPEH(base_emg.t,base_emg{:,emgCtr},base_times{i},xwindow);
-%         [adapt_av,adaptCIlow,adaptCIhigh] = getPEH(adapt_emg.t,adapt_emg{:,emgCtr},adapt_times{i},xwindow);
-%         [wash_av,washCIlow,washCIhigh] = getPEH(wash_emg.t,wash_emg{:,emgCtr},wash_times{i},xwindow);
-%         
-%         minimax(i,:) = [min([baseCIlow adaptCIlow washCIlow]) max([baseCIhigh adaptCIhigh washCIhigh])];
-%         
-%         timevec = linspace(-0.1,0.3,length(base_av));
-%         subplot(1,3,1)
-%         patch([timevec fliplr(timevec)],[baseCIlow fliplr(baseCIhigh)],colors(i,:))
-% %         plot(timevec,base_av,'color',colors(i,:))
-%         hold on
-%         set(gca,'ylim',ywindow)
-%         subplot(1,3,2)
-%         patch([timevec fliplr(timevec)],[adaptCIlow fliplr(adaptCIhigh)],colors(i,:))
-% %         plot(timevec,adapt_av,'color',colors(i,:))
-%         title(base_emg.Properties.VariableNames{emgCtr});
-%         hold on
-%         set(gca,'ylim',ywindow)
-%         subplot(1,3,3)
-%         patch([timevec fliplr(timevec)],[washCIlow fliplr(washCIhigh)],colors(i,:))
-% %         plot(timevec,wash_av,'color',colors(i,:))
-%         hold on
-%         
-%     end
-%     ywindow(1) = min(minimax(:,1));
-%     ywindow(2) = max(minimax(:,2));
-%     for i = 1:3
-%         subplot(1,3,i)
-%         set(gca,'ylim',ywindow)
-%     end
-% end
-% 
-% %% Look at EMG PCA
-% EMGIDX = contains(base_emg.Properties.VariableNames,'EMG');
-% base_bumps = [];
-% for trialCtr=1:height(base_table)
-%     base_bumps = [base_bumps; base_emg{base_emg.t>=base_table.bumpTime(trialCtr)-0.1 & base_emg.t<=base_table.bumpTime(trialCtr)+0.3,EMGIDX}];
-% end
-% 
-% adapt_bumps = [];
-% for trialCtr=1:height(adapt_table)
-%     adapt_bumps = [adapt_bumps; adapt_emg{adapt_emg.t>=adapt_table.bumpTime(trialCtr)-0.1 & adapt_emg.t<=adapt_table.bumpTime(trialCtr)+0.3,EMGIDX}];
-% end
-% 
-% wash_bumps = [];
-% for trialCtr=1:height(wash_table)
-%     wash_bumps = [wash_bumps; wash_emg{wash_emg.t>=wash_table.bumpTime(trialCtr)-0.1 & wash_emg.t<=wash_table.bumpTime(trialCtr)+0.3,EMGIDX}];
-% end
-% 
-% window = [-0.1 0.3];
-% base_bumps = catDataWindow(base_emg.t,base_emg{:,EMGIDX},base_table.bumpTime,window);
-% adapt_bumps = catDataWindow(adapt_emg.t,adapt_emg{:,EMGIDX},adapt_table.bumpTime,window);
-% wash_bumps = catDataWindow(wash_emg.t,wash_emg{:,EMGIDX},wash_table.bumpTime,window);
-% 
-% 
-% [coeff,scores,latent_eigs] = pca([base_bumps;adapt_bumps;wash_bumps]);
-% % [base_coeff,base_scores,base_scree] = pca([base_bumps]);
-% 
-% % project adapt and wash bumps into pc space
-% % center
-% % adapt_bumps_ctr = adapt_bumps - mean(adapt_bumps);
-% % wash_bumps_ctr = wash_bumps - mean(wash_bumps);
-% % 
-% % % project
-% % adapt_scores = adapt_bumps_ctr*base_coeff;
-% % wash_scores = wash_bumps_ctr*base_coeff;
-% % 
-% 
-% % do pca
-% base_idx = (1:size(base_bumps,1))';
-% adapt_idx = (base_idx(end)+1:base_idx(end)+size(adapt_bumps,1))';
-% wash_idx = (adapt_idx(end)+1:adapt_idx(end)+size(wash_bumps,1))';
-% 
-% base_scores = scores(base_idx,:);
-% adapt_scores = scores(adapt_idx,:);
-% wash_scores = scores(wash_idx,:);
-% 
-% base_scree = var(base_scores);
-% adapt_scree = var(adapt_scores);
-% wash_scree = var(wash_scores);
-% 
-% figure
-% subplot(311)
-% bar(base_scree(1:20))
-% subplot(312)
-% bar(adapt_scree(1:20))
-% subplot(313)
-% bar(wash_scree(1:20))
-% 
-% % plot bumps
-% num_bumps = 4;
-% 
-% bump_sep = 360/num_bumps;
-% 
-% colors = linspecer(num_bumps);
-% base_colors = colors(floor(base_table.bumpDir/(pi/2)+1),:);
-% adapt_colors = colors(floor(adapt_table.bumpDir/bump_sep+1),:);
-% wash_colors = colors(floor(wash_table.bumpDir/bump_sep+1),:);
-% 
-% 
-% 
-% % figure
-% % S = linspace(1,40,size(base_scores,1));
-% % for j = 1:6
-% %     subplot(2,3,j)
-% %     for i = 1:size(base_colors,1)
-% % %         idx = (i-1)*6+1:i*6;
-% % %         idx = i*6-3;
-% %         idx = (i-1)*6+j;
-% % %         plot3(base_scores(idx,1),base_scores(idx,2),base_scores(idx,3),'Color',base_colors(i,:))
-% % %         plot(base_scores(idx,1),base_scores(idx,2),'o','Color',base_colors(i,:))
-% %         hold on
-% %         scatter(base_scores(idx,1),base_scores(idx,2),S(i),base_colors(i,:))
-% %     end
-% %     grid off
-% % %     axis([-30 30 -30 30])
-% % %     axis([-5 5 -40 -30])
-% % end
-% % 
-% % 
-% % figure
-% % S = linspace(1,40,size(adapt_scores,1));
-% % for j = 1:6
-% %     subplot(2,3,j)
-% %     for i = size(adapt_colors,1)-150:size(adapt_colors,1)
-% %         
-% % %         idx = (i-1)*6+1:i*6;
-% % %         idx = i*6-3;
-% %         idx = (i-1)*6+j;
-% % %         plot3(adapt_scores(idx,1),adapt_scores(idx,2),adapt_scores(idx,3),'Color',adapt_colors(i,:))
-% % %         plot(adapt_scores(idx,1),adapt_scores(idx,2),'o','Color',adapt_colors(i,:))
-% %         hold on
-% %         scatter(adapt_scores(idx,1),adapt_scores(idx,2),S(i),adapt_colors(i,:))
-% % %         scatter3(adapt_scores(idx,1),adapt_scores(idx,2),adapt_scores(idx,3),S(i),adapt_colors(i,:))
-% %         
-% %     end
-% %     grid off
-% % %     axis([-30 30 -30 30])
-% % %     axis([-5 5 -40 -30])
-% % end
-% % 
-% % figure
-% % S = linspace(1,40,size(wash_scores,1));
-% % for j = 1:6
-% %     subplot(2,3,j)
-% %     for i = 1:size(wash_colors,1)
-% % %         idx = (i-1)*6+1:i*6;
-% % %         idx = i*6-3;
-% %         idx = (i-1)*6+j;
-% % %         plot3(wash_scores(idx,1),wash_scores(idx,2),wash_scores(idx,3),'Color',wash_colors(i,:))
-% % %         plot(wash_scores(idx,1),wash_scores(idx,2),'o','Color',wash_colors(i,:))
-% %         hold on
-% %         scatter(wash_scores(idx,1),wash_scores(idx,2),S(i),wash_colors(i,:))
-% %         
-% %     end
-% %     grid off
-% % %     axis([-30 30 -30 30])
-% % %     axis([-5 5 -40 -30])
-% % end
 
--- lib/animateBumpcurlTrials.m
+++ lib/animateBumpcurlTrials.m
@@ -1,6 +1,10 @@
 function animateBumpcurlTrials(trial_data,field,bump_colors,waitperiod,tail_length)
 td = trial_data;
 epoch_names = {'BL','AD','WO'};
+
+min_field = min(cat(1,td.(field)));
+max_field = max(cat(1,td.(field)));
+
 figure;
 
 for timeCtr = 1:size(td(1).(field),1)
@@ -17,10 +21,12 @@ for timeCtr = 1:size(td(1).(field),1)
             tail = timeCtr-tail_length:timeCtr;
             tail = max(1,tail);
     %         plot3(td(i).emg_pca(:,1),td(i).emg_pca(:,2),td(i).emg_pca(:,3),'k-','linewidth',2,'Color',bump_colors(bumpDir_idx,:))
-            plot(plot_field(tail,1),plot_field(tail,2),'-','linewidth',2,'Color',bump_colors(bumpDir_idx,:))
-            plot(plot_field(tail(end),1),plot_field(tail(end),2),'o','linewidth',2,'Color',bump_colors(bumpDir_idx,:))
+            plot(plot_field(tail,1),plot_field(tail,3),'-','linewidth',2,'Color',bump_colors(bumpDir_idx,:))
+            plot(plot_field(tail(end),1),plot_field(tail(end),3),'o','linewidth',2,'Color',bump_colors(bumpDir_idx,:))
         end
-        axis equal
+%         axis equal
+        lims = [min(min_field(1:3)) max(max_field(1:3))];
+        set(gca,'xlim',lims,'ylim',lims)
     end
     
     pause(waitperiod)
