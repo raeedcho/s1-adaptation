8e2c9aee1c62c1b4d69307575acc83bd326aa019
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2021-09-01 13:48:38 -0400

    feat(mahal): add a full plot of all the mahal curves together

--- bumpcurl_mahal.m
+++ bumpcurl_mahal.m
@@ -25,6 +25,11 @@ function bumpcurl_mahal
     trim_end = 0.5;
     num_control_shuffles = 1000;
     do_shuffle_control = false;
+    arrays_to_plot = {'M1','S1'};
+    smoothing_window_size = 10;
+    smoothing_kernel = ones(1,smoothing_window_size)/smoothing_window_size;
+    
+    mahal_curve_file=cell(length(filenames),1);
     
     for filenum = 1:length(filenames)
         td = trial_data_cell{filenum};
@@ -43,16 +48,24 @@ function bumpcurl_mahal
             td(trialnum).timevec = timevec';
         end
         
+        mahal_curve_arr = cell(1,length(spikes_in_td));
         for arraynum = 1:length(spikes_in_td)
             % check to make sure there are enough neurons
             if size(getSig(td,spikes_in_td{arraynum}),2)<=num_dims
                 continue;
             end
             
+            % check to only plot the arrays we want
+            if ~contains(spikes_in_td{arraynum},arrays_to_plot)
+                continue
+            end
+            
             true_mahal_curve = get_mahal_curve(td,struct(...
                 'signals',spikes_in_td{arraynum},...
                 'num_dims',num_dims,...
                 'num_mahal_dims',num_mahal_dims));
+            
+            initial_mahal_dist = mean(true_mahal_curve(1:floor(length(true_mahal_curve)*0.1)));
             
             % get shuffle control
             if do_shuffle_control
@@ -96,8 +109,7 @@ function bumpcurl_mahal
                 text(length(true_mahal_curve)/2,mahal_thresh,'95% CI bound','FontSize',10,'color',[0.5 0.5 0.5])
             end
             hold on
-            smoothing_window_size = 10;
-            smoothing_kernel = ones(1,smoothing_window_size)/smoothing_window_size;
+            
             smoothed_dist = conv(true_mahal_curve,smoothing_kernel,'same');
             plot(1:length(true_mahal_curve),smoothed_dist,'k','linewidth',2)
             text_idx = floor(length(true_mahal_curve)/10);
@@ -126,8 +138,69 @@ function bumpcurl_mahal
                     sanitized_datetime,...
                     strrep(spikes_in_td{arraynum},'_spikes',''))));
             end
+            
+            mahal_curve_arr{arraynum} = table(...
+                {td(1).monkey},...
+                {td(1).date_time},...
+                {strrep(spikes_in_td{arraynum},'_spikes','')},...
+                {true_mahal_curve'},...
+                initial_mahal_dist,...
+                'VariableNames',{'monkey','date_time','array','mahal_curve','initial_mahal_dist'});
         end
+        mahal_curve_file{filenum} = vertcat(mahal_curve_arr{:});
     end
+    mahal_curve_table = vertcat(mahal_curve_file{:});
+    
+    
+%% plot out all mahal curves, colored by array
+    compiled_mahal_fig = figure(...
+        'defaultaxesfontsize',10,...
+        'units','inches',...
+        'position',[3 3 6 3],...
+        'paperunits','inches',...
+        'papersize',[6 3]);
+    ax(1) = subplot(1,3,1:2);
+    
+    patch([0 0.1 0.1 0],[0 0 60 60],[0.8 0.8 0.8],'edgecolor','none')
+    hold on
+    
+    mahal_thresh = chi2inv(0.95,num_mahal_dims);
+    plot([0 1],repmat(mahal_thresh,1,2),'--','linewidth',2,'color',[0.5 0.5 0.5])
+    text(0.7,1.4*mahal_thresh,'95% CI bound','FontSize',10,'color',[0.5 0.5 0.5])
+    
+    array_colors = linspecer(length(arrays_to_plot));
+    
+    for arraynum = 1:length(arrays_to_plot)
+        [~,mahal_curve_array] = getNTidx(mahal_curve_table,'array',arrays_to_plot{arraynum});
+        for sessionnum = 1:height(mahal_curve_array)
+            temp_mahal_curve = mahal_curve_array{sessionnum,'mahal_curve'}{1};
+            plot(...
+                (1:length(temp_mahal_curve))/length(temp_mahal_curve),...
+                conv(temp_mahal_curve,smoothing_kernel,'same'),...
+                'linewidth',2,'color',array_colors(arraynum,:))
+        end
+        text(0.25*arraynum,(4-arraynum)*mahal_thresh,arrays_to_plot{arraynum},'FontSize',10,'color',array_colors(arraynum,:))
+    end
+    xlabel('Fraction into adaptation epoch')
+    ylabel('Mahal dist to final 40 trials')
+    set(gca,'box','off','tickdir','out','xtick',0.2:0.2:1,'ytick',20:20:60)
+    
+    ax(2) = subplot(1,3,3);
+    [~,array_idx] = ismember(mahal_curve_table{:,'array'},arrays_to_plot);
+    plot([0.5 0.5+length(arrays_to_plot)],repmat(mahal_thresh,1,2),'--','linewidth',2,'color',[0.5 0.5 0.5])
+    hold on
+    text(0.7,1.4*mahal_thresh,'95% CI bound','FontSize',10,'color',[0.5 0.5 0.5])
+    scatter(array_idx,mahal_curve_table{:,'initial_mahal_dist'},[],array_colors(array_idx,:),'filled')
+    ylabel('Distance in first 10% of adaptation')
+    set(gca,'box','off','tickdir','out',...
+        'xlim',[0.5 0.5+length(arrays_to_plot)],'xtick',1:length(arrays_to_plot),'xticklabel',arrays_to_plot,...
+        'ytick',20:20:60,'yticklabel',{})
+    linkaxes(ax,'y')
+    
+    if save_figures
+        saveas(compiled_mahal_fig,fullfile(figsavedir,'dpc_mahal_dist_compiled.pdf'));
+    end
+    
 end
 
 %%%% subfunctions %%%%
